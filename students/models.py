a7b8ffef3983ae79f32cf9e77ae75567411f7d42
Asocia <akkayas17@itu.edu.tr>
2021-04-19 14:29:11 +0300

    Create student model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from smart_selects.db_fields import ChainedForeignKey
+
+from sis.models import Curriculum, Faculty, Program
+from transcript.models import Transcript
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_id = models.CharField(max_length=9)
+    name = models.CharField(max_length=100, default="")
+    surname = models.CharField(max_length=100, default="")
+
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    program = ChainedForeignKey(
+        Program, chained_field="faculty", chained_model_field="faculty"
+    )
+    curriculum = ChainedForeignKey(
+        Curriculum, chained_field="program", chained_model_field="program"
+    )
+
+    transcript = models.OneToOneField(Transcript, on_delete=models.CASCADE,
+                                      null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
